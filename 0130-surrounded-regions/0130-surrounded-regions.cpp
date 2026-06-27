c9976784a815ed93d14b0231f6877a03488cb029
Mohd Saif <166315333+MohdSaif2884@users.noreply.github.com>
2026-06-28 00:58:42 +0530

    Time: 0 ms (100%), Space: 13.9 MB (80.97%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+void fix(vector<vector<char>>& board){
+     int n = board.size();
+     int m = board[0].size();
+    for(int i=0 ; i<n ; i++){
+        for(int j =0 ; j<m ; j++){
+            if(board[i][j] =='O'){
+                board[i][j] ='X';
+            }
+
+            if(board[i][j] == '2'){
+                board[i][j] ='O';
+            }
+        }
+    }
+}
+
+void dfs(vector<vector<char>>& board, int i , int j ){
+    int n = board.size();
+     int m = board[0].size();
+
+     if(i >=n || j>=m || i <0 || j <0){
+        return ;
+     }
+
+     if(board[i][j] == 'X' ||  board[i][j] == '2'){
+        return ;
+     }
+
+      board[i][j] = '2';
+      dfs(board, i+1, j);
+      dfs(board, i-1, j);
+      dfs(board, i, j+1);
+      dfs(board, i , j-1);
+}
+    void solve(vector<vector<char>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+
+        for(int i =0 ; i<n ; i ++){
+            if(board[i][0] == 'O'){
+                dfs(board, i , 0);
+            }
+
+        if(board[i][m-1]== 'O'){
+            dfs(board, i , m-1);
+        }
+        }
+
+        for(int j =0 ; j <m ; j++){
+            if(board[0][j] =='O'){
+                dfs(board, 0 , j);
+            }
+
+            if(board[n-1][j]=='O'){
+                dfs(board, n-1, j);
+            }
+        }
+
+        fix(board);
+        
+    }
+};
