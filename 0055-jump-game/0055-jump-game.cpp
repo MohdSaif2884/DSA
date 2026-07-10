bc225a2f6c69c138964942ddaec00b1526d96f04
Mohd Saif <166315333+MohdSaif2884@users.noreply.github.com>
2026-07-11 01:15:55 +0530

    Time: 0 ms (100%), Space: 52.2 MB (98.7%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,19 +2,18 @@
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        int maxReach = 0; // Shuru mein hum sirf index 0 tak jaa sakte hain
+        int maxReach = 0;  
         
         for (int i = 0; i < n; i++) {
-            // Agar main ek aise index par aa gaya jahan tak main kood hi nahi sakta tha
+             
             if (i > maxReach) {
                 return false; 
             }
             
-            // Apne maxReach ko update karo
-            // Ya toh purana maxReach bada hoga, ya naya jump (i + nums[i])
+            
             maxReach = max(maxReach, i + nums[i]);
             
-            // Optimization: Agar hum array ke end tak ya uske aage pahunch gaye, toh aage loop chalane ki zaroorat nahi
+             
             if (maxReach >= n - 1) {
                 return true;
             }
