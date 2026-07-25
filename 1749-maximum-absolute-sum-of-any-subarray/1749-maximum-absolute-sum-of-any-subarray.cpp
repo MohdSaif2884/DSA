882334e9818f47d9e02b3f4a70c438ed1a1f4373
Mohd Saif <166315333+MohdSaif2884@users.noreply.github.com>
2026-07-26 01:16:03 +0530

    Time: 0 ms (100%), Space: 45.2 MB (24.34%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -1,21 +1,18 @@
- class Solution {
+class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-
-        int n = nums.size();
-
-        long long minsum = nums[0];
-        long long maxsum = nums[0];
-        long long ans = abs(1LL * nums[0]);
-
-        for (int i = 1; i < n; i++) {
-
-            minsum = min(minsum + nums[i], 1LL * nums[i]);
-            maxsum = max(maxsum + nums[i], 1LL * nums[i]);
-
-            ans = max(ans, max(maxsum, abs(minsum)));
+int n = nums.size();
+        long long minsum =nums[0];
+        long long maxsum =nums[0];
+    //    int maxi = nums[0];
+      long long maxi = abs(1LL * nums[0]);
+        for(int i =1 ;i <n ; i++){
+            minsum = min(minsum + nums[i],(long long)nums[i] );
+            maxsum = max(maxsum + nums[i],(long long) nums[i]);
+            maxi = max(maxi , max(maxsum , abs(minsum)));
+            
         }
-
-        return (int)ans;
+        return (int) maxi;
+        
     }
 };
